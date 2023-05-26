4dacac067c7915297fade174fcc59dafad3ee430
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-05-26 08:39:51 +0200

    Edits to footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,13 @@
 import Anchor from "./Anchor";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <div className="m-auto">
-        {children}
-        <footer>Footer</footer>
-      </div>
+      <div className="m-auto">{children}</div>
+      <Footer />
     </>
   );
 }
