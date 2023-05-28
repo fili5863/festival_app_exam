31eeb4f5cd8fc9804ebdda5aaa196aea136ef1f3
TheFred94 <frederikrlarsen@gmail.com>
2023-05-28 13:53:42 +0200

    some responsiveness on the program page

--- src/components/ObjectBand.js
+++ src/components/ObjectBand.js
@@ -37,19 +37,18 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
           className="bandcontainer relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 pb-110 border-b-2 border-color-white last:border-none  md:border-none"
         >
           <Image
-            className="-z-20"
             fill={true}
             src={backgroundImage(band.act)}
             alt={band.bio}
             quality={80}
           />
           {/* --------------------------------------- */}
-          <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center z-100">
+          <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center z-40 ">
             <Checkbox
               onClick={LocalStorageFavourite}
               checked={localChecked(band.act)}
               value={band.act}
-              className="p-0"
+              className="p-0 "
               icon={<FavoriteBorder />}
               checkedIcon={<Favorite />}
               color="error"
@@ -61,10 +60,10 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
           {/* --------------------------------------- */}
           <Anchor
             href={`/bands/${bandSlug(band.act)}`}
-            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50 lg:hover:bg-opacity-0 transition"
+            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50 lg:hover:bg-opacity-0 transition z-0"
           >
-            <span className="text-color-black font-sans uppercase font-bold pt-2 md:pt-4 lg:pt-5 place-self-center w-fit px-6 mx-6 mt-20 py-1 md:py-2 lg:py-3 md:text-xl lg:text-2xl text-center bg-color-white">{band.act}</span>
-            <span className="timeslot text-color-black font-sans uppercase font-bold pt-2 place-self-center w-max px-6 mx-6 mb-20 py-1 md:text-xl lg:text-xl text-center bg-color-white lg:opacity-0 md:transition">
+            <span className="text-color-black font-sans uppercase font-bold pt-2 md:pt-4 lg:pt-5 place-self-center w-fit px-6 mx-6 mt-20 py-1 md:py-2 lg:py-3 md:text-xl lg:text-2xl text-center bg-color-white ">{band.act}</span>
+            <span className="timeslot text-color-black font-sans uppercase font-bold pt-2 place-self-center w-max px-6 mx-6 mb-20 py-1 md:text-xl lg:text-xl text-center bg-color-white lg:opacity-0 md:transition ">
               {band.start} - {band.end}
             </span>
           </Anchor>
@@ -77,14 +76,14 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
           className="relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 pb-110 border-b-2 border-color-white last:border-none  md:border-none"
         >
           <Image
-            className="-z-10"
+            className=""
             fill={true}
             src={backgroundImage(band.act)}
             alt={band.bio}
             quality={80}
           />
           {/* --------------------------------------- */}
-          <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center">
+          <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center z-40 ">
             <Checkbox
               onClick={LocalStorageFavourite}
               checked={localChecked(band.act)}
@@ -101,10 +100,10 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
           {/* --------------------------------------- */}
           <Anchor
             href={`/bands/${bandSlug(band.act)}`}
-            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50"
+            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50 "
           >
-            <span className="text-color-black font-sans uppercase font-bold md:pt-4 lg:pt-5 place-self-center w-fit px-6 mx-6 mt-20 py-1 md:py-2 lg:py-3 md:text-xl lg:text-2xl text-center bg-color-white">{band.act}</span>
-            <span className="timeslot text-color-red font-sans uppercase font-bold place-self-center w-max px-6 mx-6 mb-20 py-1 md:text-xl lg:text-xl text-center bg-color-white">{band.cancelled ? "Cancelled" : ""}</span>
+            <span className="text-color-black font-sans uppercase font-bold md:pt-4 lg:pt-5 place-self-center w-fit px-6 mx-6 mt-20 py-1 md:py-2 lg:py-3 md:text-xl lg:text-2xl text-center bg-color-white z-100">{band.act}</span>
+            <span className="timeslot text-color-red font-sans uppercase font-bold place-self-center w-max px-6 mx-6 mb-20 py-1 md:text-xl lg:text-xl text-center bg-color-white z-100">{band.cancelled ? "Cancelled" : ""}</span>
           </Anchor>
         </div>
       )
--- src/components/ObjectDay.js
+++ src/components/ObjectDay.js
@@ -26,7 +26,7 @@ export function ObjectDay({ stage, selectedDay, selectedAct, bands, LocalStorage
   //   /* Denne function gør at vi kan filtrere på hvilken dag der skal vises program for */
   // }
   return <div>
-<h3 className="md:text-3xl lg:text-5xl uppercase text-center md:mt-20 lg:mt-28 mb-7 md:mb-10 lg:mb-14">{selectedDay !== "" ? fullDayName(selectedDay) : ""}</h3>
+<h3 className="sm:mt-20 md:text-3xl lg:text-5xl uppercase text-center md:mt-20 lg:mt-28 mb-7 md:mb-10 lg:mb-14">{selectedDay !== "" ? fullDayName(selectedDay) : ""}</h3>
   {Object.keys(stage)
     .filter(day => !selectedDay || day === selectedDay)
     .map(day => {
@@ -41,7 +41,7 @@ export function ObjectDay({ stage, selectedDay, selectedAct, bands, LocalStorage
       } else {
         /* --------------------------------------- */
         return <div key={day}>
-        <h3 className="md:text-3xl lg:text-5xl uppercase text-center md:mt-20 lg:mt-28 mb-7 md:mb-10 lg:mb-14">{fullDayName(day)}</h3>
+        <h3 className=" sm:mt-20 md:text-3xl lg:text-5xl uppercase text-center md:mt-20 lg:mt-28 mb-7 md:mb-10 lg:mb-14">{fullDayName(day)}</h3>
         <div key={day} className="bandList grid sm:grid-cols-1 md:grid-cols-2 md:mb-4 lg:grid-cols-3">
         <ObjectBand days={...stage[day]} selectedAct={selectedAct} bands={bands} LocalStorageFavourite={LocalStorageFavourite} localChecked={localChecked} />
         </div>
