530e4431d21291315aaf6bf1112a6dd5b3a8cf56
TheFred94 <frederikrlarsen@gmail.com>
2023-05-29 09:53:30 +0200

    images work

--- src/components/ObjectBand.js
+++ src/components/ObjectBand.js
@@ -5,6 +5,11 @@ import React from "react";
 import { Checkbox } from "@mui/material";
 import Image from "next/image";
 
+const imageStyle = {
+  height: "100%",
+  width: "auto",
+};
+
 export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, localChecked }) {
   // console.log("days", days)
   // console.log("selectedAct", selectedAct)
@@ -34,14 +39,17 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
         <div
           key={band.act}
           // style={{ backgroundImage: backgroundImage(band.act) }}
-          className="bandcontainer relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 pb-110 border-b-2 border-color-white last:border-none  md:border-none"
+          className="bandcontainer relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 w-100 pb-110 border-b-2 border-color-white last:border-none  md:border-none"
         >
           <Image
-            src={backgroundImage(band.act)}
-            alt={band.bio}
+            style={imageStyle}
             height={1080}
             width={1920}
+            src={backgroundImage(band.act)}
+            alt={band.bio}
+            sizes="(max-width: 750px) 100vw, 750px"
             quality={80}
+            placeholder="empty"
           />
           {/* --------------------------------------- */}
           <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center z-40 ">
@@ -61,7 +69,7 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
           {/* --------------------------------------- */}
           <Anchor
             href={`/bands/${bandSlug(band.act)}`}
-            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50 lg:hover:bg-opacity-0 transition z-0"
+            className="flex flex-col w-full h-full gap-16 md:gap-0 justify-center md:justify-between bg-color-black bg-opacity-50 lg:hover:bg-opacity-0 transition z-0 absolute"
           >
             <span className="text-color-black font-sans uppercase font-bold pt-2 md:pt-4 lg:pt-5 place-self-center w-fit px-6 mx-6 mt-20 py-1 md:py-2 lg:py-3 md:text-xl lg:text-2xl text-center bg-color-white ">{band.act}</span>
             <span className="timeslot text-color-black font-sans uppercase font-bold pt-2 place-self-center w-max px-6 mx-6 mb-20 py-1 md:text-xl lg:text-xl text-center bg-color-white lg:opacity-0 md:transition ">
@@ -74,15 +82,17 @@ export function ObjectBand({ days, selectedAct, bands, LocalStorageFavourite, lo
         <div
           key={band.act}
           // style={{ backgroundImage: backgroundImage(band.act) }}
-          className="relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 pb-110 border-b-2 border-color-white last:border-none  md:border-none"
+          className="bandcontainer relative grid items-start justify-items-center bg-cover bg-no-repeat h-96 w-100 pb-110 border-b-2 border-color-white last:border-none  md:border-none "
         >
           <Image
-            className=""
+            style={imageStyle}
             height={1080}
             width={1920}
             src={backgroundImage(band.act)}
             alt={band.bio}
+            sizes="(max-width: 750px) 100vw, 750px"
             quality={80}
+            placeholder="empty"
           />
           {/* --------------------------------------- */}
           <div className="iconContainer absolute top-5 right-5 w-3 h-3 bg-color-yellow p-5 rounded-full flex items-center justify-center z-40 ">
