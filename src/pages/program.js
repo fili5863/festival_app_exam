cfcc356f15e60a623768183da621da4b11931ecb
TheFred94 <frederikrlarsen@gmail.com>
2023-05-30 11:03:58 +0200

    changed color on the personal program popup

--- src/pages/program.js
+++ src/pages/program.js
@@ -64,9 +64,9 @@ export default function Program({ schedule, bands }) {
     console.log("length", setSelectedAct.length);
   }
 
-  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const LocalStorageFavourite = e => {
+  const LocalStorageFavourite = (e) => {
     // console.log(e)
     if (snackOpen[0] === true) {
       closeSnack;
@@ -79,9 +79,7 @@ export default function Program({ schedule, bands }) {
         }
       });
     } else if (snackOpen[0] === false) {
-      e.target.checked === true
-        ? setSnackOpen([true, `${e.target.value} has been added to favourites`])
-        : setSnackOpen([true, `${e.target.value} has been removed from favourites`]);
+      e.target.checked === true ? setSnackOpen([true, `${e.target.value} has been added to favourites`]) : setSnackOpen([true, `${e.target.value} has been removed from favourites`]);
     }
     CheckFavourites(e.target.value, e.target.checked);
   };
@@ -101,7 +99,7 @@ export default function Program({ schedule, bands }) {
       if (favourites.length === 0) {
         setFavourites();
       } else {
-        const newFavourites = favourites.filter(fav => fav != `${band}/`);
+        const newFavourites = favourites.filter((fav) => fav != `${band}/`);
         setFavourites(newFavourites);
       }
     }
@@ -110,25 +108,24 @@ export default function Program({ schedule, bands }) {
 
   const action = (
     <>
-      <Anchor href="personalprogram">
-        <Button
-          color="success"
-          size="small"
-        >
-          See Personal Program
-        </Button>
+      <Anchor
+        className="my-5 "
+        href="personalprogram"
+      >
+        <Button className="text-xs  rounded-none border-2  border-solid  border-color-yellow h-10 text-color-yellow hover:bg-color-yellow hover:text-color-black font-sans font-bold">See Personal Program</Button>
       </Anchor>
       <IconButton
+        className="mx-5 bg-color-white hover:bg-color-yellow"
         size="small"
         aria-label="close"
-        color="inherit"
+        color="white"
       >
         <CloseIcon fontSize="small" />
       </IconButton>
     </>
   );
 
-  const localChecked = band => {
+  const localChecked = (band) => {
     if (favourites !== undefined) {
       for (let i = 0; i < favourites.length; i++) {
         if (favourites[i].substring(0, favourites[i].lastIndexOf("/")) === band) {
@@ -198,9 +195,7 @@ export default function Program({ schedule, bands }) {
         <title>FooFest | Program</title>
       </Head>
       <div className="max-w-screen-xl mt-10 m-auto bg-gradient-to-b from-color-black to-color-blue">
-        <h1 className="uppercase text-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
-          Program
-        </h1>
+        <h1 className="uppercase text-center text-5xl sm:text-6xl md:text-7xl lg:text-8xl">Program</h1>
         <div className="flex flex-col gap-6 mt-10 mb-20">
           <div className="flex flex-col lg:flex-row-reverse justify-center gap-2 lg:mt-10 ">
             <div className=" flex justify-center w-64 sm:w-80 md:w-96 mx-auto lg:mx-0">
@@ -259,7 +254,7 @@ export default function Program({ schedule, bands }) {
         />
         <Snackbar
           open={snackOpen[0]}
-          autoHideDuration={4000}
+          autoHideDuration={100000000}
           onClose={closeSnack}
           message={snackOpen[1]}
           action={action}
@@ -276,11 +271,7 @@ export async function getServerSideProps() {
 
   // Fetch post data from API using the ID parameter
 
-  const [res1, res2, res3] = await Promise.all([
-    fetch(`${apiUrl}/bands`),
-    fetch(`${apiUrl}/schedule`),
-    fetch(`${apiUrl}/events`),
-  ]);
+  const [res1, res2, res3] = await Promise.all([fetch(`${apiUrl}/bands`), fetch(`${apiUrl}/schedule`), fetch(`${apiUrl}/events`)]);
 
   const bands = await res1.json();
   const schedule = await res2.json();
