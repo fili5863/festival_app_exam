ff1c5ddf9ac1257813b3d0ae271a26b8b7773610
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-06-01 10:06:05 +0200

    favicon added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,17 @@
 import Anchor from "./Anchor";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="http://webdesam.dk/test/logoSmall.svg"
+        />
+      </Head>
       <Navbar />
       <div className="m-auto">{children}</div>
       <Footer />
